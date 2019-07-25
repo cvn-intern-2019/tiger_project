c7a5441bd73cb2be23081ee7c3ff8a1bdbe036fd
huynhlehieunam <huynhlehieunam@gmail.com>
2019-07-25 13:55:08 +0700

    Fix bugs
    Fix bugs

--- controllers/user/profile.controller.js
+++ controllers/user/profile.controller.js
@@ -2,21 +2,25 @@ var User = require("../../models/user.model");
 var crypto = require("crypto");
 var moment = require("moment");
 
-
 module.exports.getProfilePage = (req, res, next) => {
-    
-    let idUser = req.session._id;
-    
-    User.findById(idUser, "username email avatar fullname phone gender birthday",(err,userData)=>{
-        if(err) return next(err);
-        if(!userData) return next(new NotFound);
-        
-        //convert date to string and display in format DD-MM-YYYY
-        userData.birthday_formatted = moment(userData.birthday).format("DD-MM-YYYY");
-        res.render("user/profile", {
-            title: "View user page",
-            userData: userData,
-        });
-    })
+  let idUser = req.session.userId;
 
+  User.findById(
+    idUser,
+    "username email avatar fullname phone gender birthday",
+    (err, userData) => {
+      if (err) return next(err);
+      if (!userData) return next(new NotFound());
+
+      //convert date to string and display in format DD-MM-YYYY
+      userData.birthday_formatted = moment(userData.birthday).format(
+        "DD-MM-YYYY"
+      );
+
+      res.render("user/profile", {
+        title: "View user page",
+        userData: userData
+      });
+    }
+  );
 };
