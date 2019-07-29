f492a8b3a74f14c121cf6f7df8987a059b601f74
Poon Nguyen <nguyenduypoon@gmail.com>
2019-07-29 17:33:54 +0700

    Remove roomSocketId attribute

--- src/user/room.js
+++ src/user/room.js
@@ -1,16 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import "jquery/dist/jquery.slim";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
+import "./animate.css";
 const $ = require("jquery");
 
 $(document).ready(() => {
   $("#createRoom").hide();
   $(".search-box").hide();
 
-  
   $("#close-sidebar").click(function() {
     $(".page-wrapper").removeClass("toggled");
   });
   $("#show-sidebar").click(function() {
     $(".page-wrapper").addClass("toggled");
   });
-
-  
 });
