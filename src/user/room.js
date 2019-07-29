f69c522c98aec0adfc59803075b350ea38e001e7
Phan Thi Mai <32201095+PhanThiMai@users.noreply.github.com>
2019-07-29 17:04:06 +0700

    Edit UI of room

--- src/user/room.js
+++ src/user/room.js
@@ -3,4 +3,14 @@ const $ = require("jquery");
 $(document).ready(() => {
   $("#createRoom").hide();
   $(".search-box").hide();
+
+  
+  $("#close-sidebar").click(function() {
+    $(".page-wrapper").removeClass("toggled");
+  });
+  $("#show-sidebar").click(function() {
+    $(".page-wrapper").addClass("toggled");
+  });
+
+  
 });
