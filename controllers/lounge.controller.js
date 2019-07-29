236f7897b5055156e7ce396f4d83a00d1f741d5f
huynhlehieunam <huynhlehieunam@gmail.com>
2019-07-30 00:03:21 +0700

    Search room function
    - Implement search by room name and host name.

--- controllers/lounge.controller.js
+++ controllers/lounge.controller.js
@@ -3,42 +3,50 @@ module.exports.getRoomPage = (req, res, next) => {
     {
       roomID: 1,
       roomAuthor: "phanmai",
-      roomNumPlayers: 10
+      roomNumPlayers: 5,
+      totalPlayers: 7
     },
     {
       roomID: 2,
       roomAuthor: "test1",
-      roomNumPlayers: 8
+      roomNumPlayers: 5,
+      totalPlayers: 7
     },
     {
       roomID: 3,
       roomAuthor: "maiphan2",
-      roomNumPlayers: 10
+      roomNumPlayers: 5,
+      totalPlayers: 7
     },
     {
       roomID: 4,
       roomAuthor: "maiphan4",
-      roomNumPlayers: 8
+      roomNumPlayers: 5,
+      totalPlayers: 7
     },
     {
       roomID: 5,
       roomAuthor: "test2",
-      roomNumPlayers: 10
+      roomNumPlayers: 5,
+      totalPlayers: 7
     },
     {
       roomID: 6,
       roomAuthor: "m123245",
-      roomNumPlayers: 8
+      roomNumPlayers: 5,
+      totalPlayers: 7
     },
     {
       roomID: 7,
       roomAuthor: "phanmai0",
-      roomNumPlayers: 10
+      roomNumPlayers: 5,
+      totalPlayers: 7
     },
     {
       roomID: 8,
       roomAuthor: "Nhat",
-      roomNumPlayers: 8
+      roomNumPlayers: 5,
+      totalPlayers: 7
     }
   ];
   let username = req.session.username;
--- src/lounge.js
+++ src/lounge.js
@@ -1,1 +1,26 @@
 import "./layout";
+import { restProperty } from "babel-types";
+
+const $ = require('jquery');
+
+let searchRoomEvent = function(){
+    let keyword = $("#search_input").val();
+
+    $("#room_area>div")
+    .hide()
+    .each(function(index,element){
+        let roomName = $(element).find(".room_name").text().toLowerCase();
+        let roomAuthor = $(element).find(".room_author").text().toLowerCase();
+
+        if(
+            roomName.search(keyword) != -1 ||
+            roomAuthor.search(keyword) != -1
+        ){
+            $(element).fadeIn(200);
+        }
+    });
+}
+
+$(document).ready(function () {
+    $("#search_input").keyup(searchRoomEvent)
+});
