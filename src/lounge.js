7819e55656dc353f16bae8684ae3609f90b08f08
huynhlehieunam <huynhlehieunam@gmail.com>
2019-07-30 09:32:16 +0700

    fix errors on search name.
    - lowercase search keywords.

--- src/lounge.js
+++ src/lounge.js
@@ -1,26 +1,32 @@
 import "./layout";
 import { restProperty } from "babel-types";
 
-const $ = require('jquery');
+const $ = require("jquery");
 
-let searchRoomEvent = function(){
-    let keyword = $("#search_input").val();
+let searchRoomEvent = function() {
+  let keyword = $("#search_input")
+    .val()
+    .toLowerCase();
 
-    $("#room_area>div")
+  $("#room_area>div")
     .hide()
-    .each(function(index,element){
-        let roomName = $(element).find(".room_name").text().toLowerCase();
-        let roomAuthor = $(element).find(".room_author").text().toLowerCase();
+    .each(function(index, element) {
+      let roomName = $(element)
+        .find(".room_name_id")
+        .text()
+        .toLowerCase();
 
-        if(
-            roomName.search(keyword) != -1 ||
-            roomAuthor.search(keyword) != -1
-        ){
-            $(element).fadeIn(200);
-        }
+      let roomAuthor = $(element)
+        .find(".room_author")
+        .text()
+        .toLowerCase();
+
+      if (roomName.search(keyword) != -1 || roomAuthor.search(keyword) != -1) {
+        $(element).fadeIn(200);
+      }
     });
-}
+};
 
-$(document).ready(function () {
-    $("#search_input").keyup(searchRoomEvent)
+$(document).ready(function() {
+  $("#search_input").keyup(searchRoomEvent);
 });
