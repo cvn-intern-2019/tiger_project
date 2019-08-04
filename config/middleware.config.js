704659de183f691e64c7117080bc56a3a9bf4add
Poon Nguyen <nguyenduypoon@gmail.com>
2019-08-04 10:39:46 +0700

    Update favicon

--- config/middleware.config.js
+++ config/middleware.config.js
@@ -3,21 +3,26 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
+const favicon = require("serve-favicon");
 
 module.exports = app => {
   // view engine setup
-  app.set("views", path.join(__dirname, "views"));
+  app.set("views", path.join(__dirname, "../views"));
   app.set("view engine", "pug");
 
   app.use(logger("dev"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.static(path.join(__dirname, "../public")));
   app.use((req, res, next) => {
     res.set("Cache-Control", "no-store, no-cache, must-revalidate, private");
     next();
   });
+
+  app.use(
+    favicon(path.join(__dirname, "../public", "images", "werewolflogo.png"))
+  );
 
   //session setup
   app.use(
