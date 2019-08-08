8c7b23a5de02c0369a4755e3be2d02ecc96fc645
Poon Nguyen <nguyenduypoon@gmail.com>
2019-08-08 11:09:12 +0700

    Add .env for config enviroment

--- config/database.config.js
+++ config/database.config.js
@@ -1,12 +1,16 @@
 var mongoose = require("mongoose");
 var Grid = require("gridfs-stream");
 var GridFsStorage = require("multer-gridfs-storage");
+require("dotenv").config();
 
 mongoose.set("useCreateIndex", true);
 mongoose.set("useFindAndModify", false);
 mongoose.set("useNewUrlParser", true);
 mongoose.Promise = global.Promise;
-const connStr = "mongodb://tiger:tiger123@localhost:27017/werewolf";
+
+const connStr = `mongodb://${process.env.DB_USERNAME}:${
+  process.env.DB_PASSWORD
+}@${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 module.exports.init = () => {
   mongoose.connect(connStr, err => {
